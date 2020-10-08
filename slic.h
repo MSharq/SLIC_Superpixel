dc0f4a32815e2ad0312878aaed030522c9a5a67e
Sharique <sharique@ipmlan.ipm.fraunhofer.de>
2020-10-08 16:32:24 +0200

    added global static filepath variables

--- slic.h
+++ slic.h
@@ -3,22 +3,29 @@
 #include <opencv2/core.hpp>
 #include <array>
 
+#include <iostream>
+
+// REMEMBER TO FILL UP THE RIGHT PATHS
+static std::string inputFile = "C:\\Users\\sharique\\Desktop\\Test-Bild-Cork\\rsz_rsz_002.bmp";
+static std::string outputFile_mean = "C:\\Users\\sharique\\Desktop\\Test-Bild-Cork\\SLIC_mean.png";
+static std::string outputFile_redLine = "C:\\Users\\sharique\\Desktop\\Test-Bild-Cork\\SLIC_redLine.png";
+
 namespace superpixel {
     struct PixelFeature {
-        //Constructor
-        PixelFeature(): labValue{}, xy(0,0) { }
+        //Constructor, {} is for list, () is for direct values intializations
+        PixelFeature() : labValue{}, xy(0, 0) { }
         //Public variables
         std::array<double, 3> labValue;
         cv::Point xy;
     };
 
     class SLIC {
-     public:
-      void getSupperpixels(cv::Mat input, const int numOfSuperpixels, const int compactness, const double treshold);
-     private:
-      double distance(PixelFeature f1, PixelFeature f2, const int compactness, const int S);
-      cv::Mat enforceConnectivity(cv::Mat labels, const int numOfSuperpixels);
-      cv::Point getLocalMinimum(cv::Mat input, const int x, const int y, const int n);
+    public:
+        void getSupperpixels(cv::Mat input, const int numOfSuperpixels, const int compactness, const double treshold);
+    private:
+        double distance(PixelFeature f1, PixelFeature f2, const int compactness, const int S);
+        cv::Mat enforceConnectivity(cv::Mat labels, const int numOfSuperpixels);
+        cv::Point getLocalMinimum(cv::Mat input, const int x, const int y, const int n);
     };
 }
 #endif // SLIC_H
