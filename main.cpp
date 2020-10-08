cb40103417e1c7c91447dcd4a7ab98dc3acdef92
Sharique <sharique@ipmlan.ipm.fraunhofer.de>
2020-10-08 16:33:55 +0200

    updated code

--- main.cpp
+++ main.cpp
@@ -1,45 +1,23 @@
 #include <iostream>
-#include <unistd.h>
+//#include <unistd.h>
 #include "slic.h"
 #include <opencv2/imgcodecs.hpp>
-
-void usage()
-{
-    std::cout << "Usage: SLIC_Superpixel -i inputFile -o outputFile" << std::endl;
-}
 
 int main(int argc, char *argv[])
 {
     extern char *optarg;
     extern int optopt;
-    std::string inputFile, outputFile;
     bool errorOpt = false;
-    int c;
-    while ((c = getopt(argc, argv, ":i:o:")) != -1) {
-        switch (c) {
-        case 'i':
-            inputFile = optarg;
-            break;
-        case 'o':
-            outputFile = optarg;
-            break;
-        case ':':
-            std::cout << "Option -" << (char)optopt << " requires filename." << std::endl;
-            errorOpt = true;
-            break;
-        case '?':
-            std::cout << "Unrecognized option: -" << (char)optopt << std::endl;
-            errorOpt = true;
-            break;
-        }
-    }
-
-    if (errorOpt || argc < 5) {
-        usage();
-        return -1;
-    }
-
+    
     cv::Mat inputMatrix = cv::imread(inputFile);
+    if (inputMatrix.empty())
+    {
+        std::cout << "\ninvalid path";
+        return 1;
+    }
+
+    std::cout << "the image size "<< inputMatrix.size() << std::endl;
+
     const int numberOfSupperpixels = 400;
     const int compactness = 20;
     const double treshold = 30.0;
