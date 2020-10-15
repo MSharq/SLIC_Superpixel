83537bcaf5d4c2244b5cf4fee443713cec76ee9b
Sharique <sharique@ipmlan.ipm.fraunhofer.de>
2020-10-15 10:29:59 +0200

    added timing measurement

--- main.cpp
+++ main.cpp
@@ -3,12 +3,17 @@
 #include "slic.h"
 #include <opencv2/imgcodecs.hpp>
 
+#include <chrono>
+
 int main(int argc, char *argv[])
 {
     extern char *optarg;
     extern int optopt;
     bool errorOpt = false;
     
+    // Get starting timepoint 
+    auto start = std::chrono::high_resolution_clock::now();
+
     cv::Mat inputMatrix = cv::imread(inputFile);
     if (inputMatrix.empty())
     {
@@ -23,5 +28,15 @@ int main(int argc, char *argv[])
     const double treshold = 30.0;
     superpixel::SLIC slic;
     slic.getSupperpixels(inputMatrix, numberOfSupperpixels, compactness, treshold);
+    // Get ending timepoint 
+    auto stop = std::chrono::high_resolution_clock::now();
+
+    // Get duration. Substart timepoints to  
+    // get durarion. To cast it to proper unit 
+    // use duration cast method 
+    auto duration = std::chrono::duration_cast<std::chrono::seconds>(stop - start);
+
+    std::cout << "Time taken by function: "
+        << duration.count() << " seconds" << std::endl;
     return 0;
 }
